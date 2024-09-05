3b5e73e0b436fe800842f0b8ff511ff462b7b214
LeonardoFate <alvillamarf@gmail.com>
2024-09-04 19:53:03 -0500

    Conexion base de datos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const express =require('express');
+const express = require("express");
+const mongodb = require("mongoose");
+require("dotenv").config();
 const port = process.env.PORT || 9000;
 const app = express();
 
 //Rutas
-app.get('/', (req, res) =>{
-res.send('Bienvenidos a mi aplicacion');
+app.get('/', (req, res) => {
+    res.send('Bienvenidos a mi aplicacion');
 })
 
-app.listen(9000,() => console.log('Servidor funcionando en el puerto', port));
+//conexion base de datos
+mongodb
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Conexion a la base exitosa"))
+    .catch((error) => console.log(error));
+
+
+
+
+
+
+//puerto
+app.listen(9000, () => console.log('Servidor funcionando en el puerto', port));
